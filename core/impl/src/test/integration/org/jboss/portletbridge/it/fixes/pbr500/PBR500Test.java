3de7fe1efdeb1354843575b7db600ca01ad9cadb
Ken Finnigan <ken@kenfinnigan.me>
2013-09-05 07:21:38 -0400

    Create common directory for fixes related to jiras

--- core/impl/src/test/integration/org/jboss/portletbridge/it/fixes/pbr500/PBR500Test.java
+++ core/impl/src/test/integration/org/jboss/portletbridge/it/fixes/pbr500/PBR500Test.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.portletbridge.it.pbr500;
+package org.jboss.portletbridge.it.fixes.pbr500;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
